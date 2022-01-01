a4af3cf0be3e935adfe312057488280a270bc8c4
Jusa Mankki <jusamankki@gmail.com>
2022-01-01 22:49:42 +0200

    replace duplicates on websocket messages

--- client/src/components/Live.js
+++ client/src/components/Live.js
@@ -8,16 +8,33 @@ const Live = () => {
 
     useEffect(() => {
         const socket = new WebSocket(link)
+
+        const gamesIncludeNewGame = (id) => {
+            return games.map(g => g.gameId).includes(id)
+        }
+
+        const replaceGameInGames = (id, newGame) => {
+            const arr_idx = games.findIndex(g => g.gameId === id)
+            const newArray = games
+            newArray[arr_idx] = newGame
+            return newArray
+        }
+
         socket.onmessage = ({ data }) => {
-            const newGame = JSON.parse(JSON.parse(data)) //ok
-            console.log(newGame.gameId)
-            //setGames(games.concat(newGame))
-            setGames(games.concat(newGame))
+            const newGame = JSON.parse(JSON.parse(data))
+            const id = newGame.gameId
+            if (!gamesIncludeNewGame(id)) {
+                setGames(games.concat(newGame))
+            } else {
+                const newArray = replaceGameInGames(id, newGame)
+                setGames(newArray)
+            }
         }
         return () => {
             socket.close()
         }
     }, [games])
+
 
     // useEffect(() => {
     //     const fetchData = async () => {
