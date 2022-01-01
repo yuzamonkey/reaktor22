56b79a27527fbae1ac26612272b764f004802973
Jusa Mankki <jusamankki@gmail.com>
2022-01-01 22:33:06 +0200

    web socket handling

--- client/src/components/Live.js
+++ client/src/components/Live.js
@@ -1,28 +1,40 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const Live = () => {
-    const ws = "ws://bad-api-assignment.reaktor.com/rps/live"
-    const socket = new WebSocket(ws)
+    const [games, setGames] = useState([])
 
-    socket.onmessage = ({ data }) => {
-        console.log("message from socket: ", data)
-    }
+    const link = "ws://bad-api-assignment.reaktor.com/rps/live"
 
     useEffect(() => {
-        const fetchData = async () => {
-            const url = "http://localhost:3001/api/history"
-            const result = await axios.get(url)
-            console.log(result.data);
-        };
-        fetchData();
-    }, []);
+        const socket = new WebSocket(link)
+        socket.onmessage = ({ data }) => {
+            const newGame = JSON.parse(JSON.parse(data)) //ok
+            console.log(newGame.gameId)
+            //setGames(games.concat(newGame))
+            setGames(games.concat(newGame))
+        }
+        return () => {
+            socket.close()
+        }
+    }, [games])
+
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const url = "http://localhost:3001/api/history"
+    //         const result = await axios.get(url)
+    //         console.log(result.data);
+    //     };
+    //     fetchData();
+    // }, []);
 
     return (
         <div className="live-component-container">
             <h2>LIVE</h2>
+            {games.map(g => <div>{g.gameId}</div>)}
         </div>
     )
 }
+
 
 export default Live
