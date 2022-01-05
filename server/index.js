f9b9e4c2eb368423431c44efb531f788bf880715
Jusa Mankki <jusamankki@gmail.com>
2022-01-05 21:49:18 +0200

    add Procfile and env port

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.get('/api/history/:cursor/:id', async (req, res) => {
     res.send(game)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
