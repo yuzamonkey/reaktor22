6561efde129f2b35e9bf4415cbd08504d7bb79e2
Jusa Mankki <jusamankki@gmail.com>
2022-01-05 22:21:32 +0200

    build client and deploy

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,3 @@
-export const API_URL = 'http://localhost:3001/api/history/'
+//export const API_URL = 'http://localhost:3001/api/history/'
+export const API_URL = '/api/history/'
 export const WS_URL = 'ws://bad-api-assignment.reaktor.com/rps/live'
